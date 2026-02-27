1dc9e16a32a33c8a6615e10d686a6f68fbdec8ab
Th-Shanjit <th.shanjit@gmail.com>
2026-02-28 01:13:56 +0530

    fixed the page layout

--- app/data/Navigation.tsx
+++ app/data/Navigation.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { cmsData } from './data/content';
+import { cmsData } from './content';
 
 export default function Navigation() {
   const pathname = usePathname();
